cdf000f4580555fcedb1472f9fc9ff2cf8bfd371
Dominik Ludwiczak <ludek077@gmail.com>
2020-04-05 10:52:03 +0000

    mailto

--- resources/views/emails/order.blade.php
+++ resources/views/emails/order.blade.php
@@ -168,7 +168,7 @@ background-color: #f6f6f6;
                     <table width="100%" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                         <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                             <td class="aligncenter content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; vertical-align: top; color: #999; text-align: center; margin: 0; padding: 0 0 20px;" tem-align="center" valign="top">
-                                Email <a href="mailto:{{Auth::user()->email}}" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0;">{{Auth::user()->email}}</a>
+                                Email <a href="mailto:phumarta.sklep@gmail.com" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0;">phumarta.sklep@gmail.com</a>
                             </td>
                         </tr>
                     </table>
