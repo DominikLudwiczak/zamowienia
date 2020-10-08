fee9811c342c167d510f47bc6bc32c6dd3b4e7c0
DominikLudwiczak <dominik.ludwiczak.priv@gmail.com>
2020-10-08 13:16:30 +0200

    messages

--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -1,7 +1,7 @@
 
 @if(count($errors) > 0)
     @foreach($errors->all() as $error)
-        <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;" class="mt-2 mr-5">
+        <div aria-live="polite" aria-atomic="true" style="position: absolute; min-height: 200px; right: 10%;" class="mt-2 mr-5">
             <div style="position: absolute; top: 0; right: 0; z-index:2;">
                 <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" type='info' data-delay="3000" style="min-width:15rem;">
                     <div class="toast-header text-white" style='background-color: #d30303;'>
@@ -17,7 +17,7 @@
 @endif
 
 @if(session('success'))
-    <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;" class="mt-2">
+    <div aria-live="polite" aria-atomic="true" style="position: absolute; min-height: 200px; right: 10%;" class="mt-2">
         <div style="position: absolute; top: 0; right: 0; z-index:2;">
             <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" type='info' data-delay="3000" style="min-width:15rem;">
                 <div class="toast-header text-white" style='background-color: #00991e;'>
@@ -32,7 +32,7 @@
 @endif
 
 @if(session('failed'))
-    <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;" class="mt-2">
+    <div aria-live="polite" aria-atomic="true" style="position: absolute; min-height: 200px; right: 10%;" class="mt-2">
         <div style="position: absolute; top: 0; right: 0; z-index:2;">
             <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" type='info' data-delay="3000" style="min-width:15rem;">
                 <div class="toast-header text-white" style='background-color: #d30303;'>
